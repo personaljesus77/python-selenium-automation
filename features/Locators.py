897e46382d7dd177c05c7c3947a17407152d7c3c
Oleg Molchanov <oleg@molchanov.us>
2019-10-06 04:48:44 -0500

    git push

---
+++ features/Locators.py
@@ -0,0 +1,40 @@
+""" Amazon logo
+Search by class Name = "a-icon a-icon-logo"
+
+
+"""
+""""Continue button
+Search by input ID = "continue"
+
+
+"""
+""""" Need help link
+Search by class Name ="a-expander-prompt"
+
+
+"""
+""""" Forgot your password link
+Search by ID ="auth-fpp-link-bottom""
+
+
+"""
+"""Other issues with Sign-In link
+Search by ID ="ap-other-signin-issues-link"""
+
+
+"""
+"""Create your Amazon account button
+Search by ID = "createAccountSubmit"
+
+
+"""
+"""Conditions of use link
+Search by href="/gp/help/customer/display.html/ref=ap_desktop_footer_cou?ie=UTF8&amp;nodeId=508088"
+
+   
+
+"""
+""" Privacy Notice link
+Search by href="/gp/help/customer/display.html/ref=ap_desktop_footer_privacy_notice?ie=UTF8&amp;nodeId=468496"
+
+
